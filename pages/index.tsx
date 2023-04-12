66448b6ba0f97f718bd8f6f2bb6589cb09a11824
Phyper14 <martins14.pablo@gmail.com>
2023-04-12 14:56:06 -0300

    refactor globalstyles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,6 @@ interface Props {
   as: string
 }
 
-const GlobalStyle = () => {
-  return (
-    <style global jsx>
-      { `
-        body {
-          font-family: sans-serif;
-        }
-      `}
-    </style>
-  )
-}
 const Title = ({ children, as }: Props) => {
   const Tag = as;
   return (
@@ -33,7 +22,6 @@ const Title = ({ children, as }: Props) => {
 export default function Home() {
   return (
     <div>
-      <GlobalStyle />
       <Title as="h1">Alura Cases - Home Page</Title>
       <Link href="faq">Ir para o Faq</Link>
     </div>
---
+++ src/Theme/GlobalStyle.tsx
@@ -0,0 +1,13 @@
+const GlobalStyle = () => {
+    return (
+        <style global jsx>
+            { `
+          body {
+            font-family: sans-serif;
+          }
+        `}
+        </style>
+    )
+}
+
+export default GlobalStyle
